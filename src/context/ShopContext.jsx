ff76fd5e4a61fedec3d7d702aaef3242e39fd3db
Ankit Shukla <Ankit.Shukla128476@gmail.com>
2025-03-11 17:26:23 +0530

    ...

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -9,6 +9,13 @@ function ShopContextProvider({ children }) {
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [products, setProducts] = useState(ShopProducts);
+    const [cartItems, setCartItems] = useState({});
+
+    const addtocart = async (itemId, itemSize) => {
+
+        let cartData = structuredClone(cartItems);
+
+    }
 
     const value = {
         products, ShopProducts, currency, setCurrency, deliveryFee, setDeliveryFee, search, setSearch, showSearch, setShowSearch
